4be19c83a3c04b7dd7b5ecc6df405cc87f8fd669
yanwenchang <yanwenchang>
2019-05-07 20:44:26 +0800

    提交文件

---
+++ src/main/java/com/yanwc/yanwcspringboot/YanwcSpringBootApplication.java
@@ -0,0 +1,24 @@
+package com.yanwc.yanwcspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *yanwenchang
+ */
+@SpringBootApplication
+public class YanwcSpringBootApplication {
+    public static void main(String[] args) {
+       SpringApplication.run(YanwcSpringBootApplication.class, args);
+       System.out.println("(♥◠‿◠)ﾉﾞ  Yanwc SpringBoot 启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
